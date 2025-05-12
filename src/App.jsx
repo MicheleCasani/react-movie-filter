96c63596a15789672b9d1b5ae459c65e3881145c
MicheleCasani <casanimichele@gmail.com>
2025-05-12 16:02:17 +0200

    [UPD]

--- src/App.jsx
+++ src/App.jsx
@@ -16,22 +16,55 @@ function App() {
 
   const [search, setSearch] = useState('')
   const [filteredFilms, setFilteredFilms] = useState(films)
+  const [newFilm, setNewFilm] = useState('')
+  const [filmsState, setFilmsState] = useState(films)
 
+  const addFilm = (event) => {
+    event.preventDefault();
+    const trimmedFilm = newFilm.trim();
+    if (
+      trimmedFilm === ''
+    )
+      return
+
+    const newFilmObj = {
+      title: trimmedFilm,
+      genre: 'genere sconosciuto'
+    }
+
+    setFilmsState([...filmsState, newFilmObj])
+    setNewFilm('');
+
+  }
 
   useEffect(() => {
-    const filteredArray = films.filter((film) => {
+    const filteredArray = filmsState.filter((film) => {
       return (
         film.genre.toLowerCase().includes(search.toLowerCase()) || film.title.toLowerCase().includes(search.toLowerCase())
       );
     })
     setFilteredFilms(filteredArray)
 
-  }, [search]);
+  }, [search, filmsState]);
 
   return (
     <>
       <div className='container mt-5'>
         <h1>Film Form</h1>
+
+        <form onSubmit={addFilm} className='mb-4'>
+          <div className='input-group'>
+            <input
+              value={newFilm}
+              onChange={(event) => {
+                setNewFilm(event.target.value)
+              }}
+              type='text'
+              placeholder='Nuovo Film'
+              className='form-control' />
+            <button className='btn btn-outline-success'>Aggiungi</button>
+          </div>
+        </form>
 
         <ul className='list-group'>
           {filteredFilms.map((film, index) =>
